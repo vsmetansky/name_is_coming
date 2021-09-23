3c47326d6ce3c722fbab3052b3c70672f8d77db8
v <vladsmetansky@gmail.com>
2021-09-23 09:36:15 +0300

    support for synchronous cache

--- name_is_coming/storage/cache.py
+++ name_is_coming/storage/cache.py
@@ -1,21 +1,43 @@
-from collections import defaultdict, deque
 from typing import Dict, Tuple
+from abc import ABC, abstractmethod
 
 import aioredis
+import redis
+
+from name_is_coming.tle import group_latest_by_name
 
 
 class KeyGenerator:
     cache_key: str = 'tle_cache'
 
 
-class RedisCache:
+class Cache(ABC):
+    @abstractmethod
+    def update(self, entries: Tuple[Dict[str, str]]):
+        pass
+
+    @abstractmethod
+    def clear(self):
+        pass
+
+    @abstractmethod
+    def retrieve(self) -> Dict[str, str]:
+        pass
+
+    @abstractmethod
+    def is_empty(self) -> bool:
+        pass
+
+
+class RedisCache(Cache):
     def __init__(self, redis_url: str):
+        super().__init__()
         self._redis = aioredis.from_url(redis_url, decode_responses=True)
 
     async def update(self, entries: Tuple[Dict[str, str]]):
         entries_to_update = await self._redis.hgetall(KeyGenerator.cache_key)
 
-        entries = self._latest_entries_by_name(entries)
+        entries = group_latest_by_name(entries)
         entries_to_update.update(entries)
 
         await self._redis.hmset(KeyGenerator.cache_key, entries_to_update)
@@ -29,12 +51,25 @@ class RedisCache:
     async def is_empty(self) -> bool:
         return bool(await self.retrieve())
 
-    @staticmethod
-    def _latest_entries_by_name(entries: Tuple[Dict[str, str]]) -> Dict[str, str]:
-        grouped = defaultdict(deque)
 
-        for entry in entries:
-            (object_name, tle_value), = entry.items()
-            grouped[object_name].append(tle_value)
+class RedisCacheSync(Cache):
+    def __init__(self, redis_url: str):
+        super().__init__()
+        self._redis = redis.from_url(redis_url, decode_responses=True)
 
-        return {name: entries.pop() for name, entries in grouped.items()}
+    def update(self, entries: Tuple[Dict[str, str]]):
+        entries_to_update = self._redis.hgetall(KeyGenerator.cache_key)
+
+        entries = group_latest_by_name(entries)
+        entries_to_update.update(entries)
+
+        self._redis.hmset(KeyGenerator.cache_key, entries_to_update)
+
+    def clear(self):
+        self._redis.delete(KeyGenerator.cache_key)
+
+    def retrieve(self) -> Dict[str, str]:
+        return self._redis.hgetall(KeyGenerator.cache_key)
+
+    def is_empty(self) -> bool:
+        return bool(self.retrieve())
--- name_is_coming/tle.py
+++ name_is_coming/tle.py
@@ -1,8 +1,10 @@
 __all__ = (
     'from_triplet',
-    'to_triplet'
+    'to_triplet',
+    'group_latest_by_name'
 )
 
+from collections import defaultdict, deque
 from typing import Tuple, Dict
 
 TLE_LINE_SEP = '\n'
@@ -16,3 +18,13 @@ def from_triplet(tle_0: str, tle_1: str, tle_2: str) -> Dict[str, str]:
 def to_triplet(tle: Dict[str, str]) -> Tuple[str, ...]:
     tle_value, = tle.values()
     return tuple(tle_value.split(TLE_LINE_SEP))
+
+
+def group_latest_by_name(tles: Tuple[Dict[str, str]]) -> Dict[str, str]:
+    grouped = defaultdict(deque)
+
+    for tle in tles:
+        (object_name, tle_value), = tle.items()
+        grouped[object_name].append(tle_value)
+
+    return {name: tles.pop() for name, tles in grouped.items()}
