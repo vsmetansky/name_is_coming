5c831faf4d567e5f5a674138d0123fd65068eb3c
v <vladsmetansky@gmail.com>
2021-10-01 14:13:59 +0300

    fixed settings to get data for all objects

--- name_is_coming/settings.py
+++ name_is_coming/settings.py
@@ -9,7 +9,7 @@ API_AUTH_URL = 'https://www.space-track.org/ajaxauth/login'
 
 API_POLL_URL_TMPL = \
     'https://www.space-track.org/basicspacedata/query/class/gp' \
-    '/decay_date/null-val/OBJECT_TYPE/Debris/epoch/>now-{}/orderby/EPOCH asc/format/json'
+    '/decay_date/null-val/epoch/>now-{}/orderby/EPOCH asc/format/json'
 API_POLL_INTERVAL = int(getenv('API_POLL_INTERVAL', 20))  # seconds
 API_POLL_LOOKBACK = int(getenv('API_POLL_LOOKBACK', 1))  # days
 API_POLL_LOOKBACK_WHEN_EMPTY = int(getenv('API_POLL_LOOKBACK_WHEN_EMPTY', 30))  # days
