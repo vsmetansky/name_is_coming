f72061dde3f8f39b006bbd684d5e9887213da027
v <vladsmetansky@gmail.com>
2021-10-03 18:02:31 +0300

    updated .gitignore

---
+++ name_is_coming/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def degree2radians(degree):
+    return degree * np.pi / 180
+
+
+def map_to_sphere(lon, lat, radius):
+    lon = np.array(lon, dtype=np.float64)
+    lat = np.array(lat, dtype=np.float64)
+    lon = degree2radians(lon)
+    lat = degree2radians(lat)
+    xs = radius * np.cos(lon) * np.cos(lat)
+    ys = radius * np.sin(lon) * np.cos(lat)
+    zs = radius * np.sin(lat)
+    return xs, ys, zs
